a01503d468c6972cc61e44950a7dedf7a9359331
Vincent Lam <vincentlam@live.com.au>
2020-11-03 07:39:16 +1030

    update reader

--- dtb/reader.py
+++ dtb/reader.py
@@ -2,15 +2,19 @@ import requests
 from io import StringIO
 
 
+def url_to_stringio(url, **kwargs):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20200101 Firefox/82.0"
+    }
+    if "headers" in kwargs:
+        headers.update(kwargs["headers"])
+    req = requests.get(url, headers=headers)
+    return StringIO(req.text)
+
+
 def pd_input(uri, **kwargs):
     assert isinstance(uri, str), "uri needs to be of str type!"
     if uri.startswith("http"):
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"
-        }
-        if "headers" in kwargs:
-            headers.update(kwargs["headers"])
-        req = requests.get(uri, headers=headers)
-        return StringIO(req.text)
+        return url_to_stringio(uri, **kwargs)
     else:
         return uri
